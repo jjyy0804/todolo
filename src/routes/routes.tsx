2ba213e905a20ef181339cd3e2c64057d1f631cf
ara jo <ara.jo.song@gmail.com>
2024-10-08 21:38:25 +0900

    Feat: add react-dom-router and connected with each components

---
+++ src/routes/routes.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Board from '../components/Board';
+import Landing from '../components/Landing';
+import Login from '../components/Login';
+import Register from '../components/Register';
+import MyPage from '../components/MyPage';
+import Calendar from '../components/Calendar';
+import ErrorPage from '../components/ErrorPage';
+
+const routes = [
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Landing />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'main',
+        element: <Board />,
+      },
+      {
+        path: 'myPage',
+        element: <MyPage />,
+      },
+      {
+        path: 'calendar',
+        element: <Calendar />,
+      },
+    ],
+  },
+  { path: '*', element: <ErrorPage /> },
+];
+
+const router = createBrowserRouter(routes);
+
+export default router;
