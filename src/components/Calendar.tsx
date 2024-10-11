f870f1a454ae664a0c33c9c9269497513ac4f7cd
ara jo <ara.jo.song@gmail.com>
2024-10-12 00:09:08 +0900

    Feat: 추가 Calendar 컴포넌트 및 스타일 적용

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,39 @@
 import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import NavigationBar from './common/NavigationBar';
+import '../calendar.css';
+
+// import useUserStore from '../store';
+// import axios, { AxiosError, AxiosResponse } from 'axios';
+
 export default function Calendar() {
-  return <p>Calendar Page ...</p>;
+  // const tasks = [];
+
+  // axios
+  //   .get('/')
+  //   .then(function (response: AxiosResponse<T>) {
+  //     // handle success
+  //     console.log(response);
+  //   })
+  //   .catch(function (error: AxiosError) {
+  //     console.log('Calendar get Data Error...', error);
+  //   })
+  //   .finally(function () {
+  //     // always executed
+  //   });
+
+  return (
+    <>
+      <NavigationBar />
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        dayMaxEvents={true}
+        // events={tasks}
+        // height={'900px'}
+        editable={true}
+      />
+    </>
+  );
 }
