a161d1e3e7656de1f74585cdd1928b291f3338d0
ara jo <ara.jo.song@gmail.com>
2024-10-16 10:55:48 +0900

    Feat: eslint config 정리

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,18 +18,9 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-<<<<<<< HEAD
     'prettier/prettier': 'warn',
     'no-undef': 'warn', // This will trigger a warning instead of an error
     'no-unused-vars': 'warn', // Will now trigger a warning instead of an error
-=======
-    'prettier/prettier': 'error',
-<<<<<<< HEAD
->>>>>>> 1fb0d03db95e03c8369cbae5321f07ddfc314b94
-=======
-    'no-undef': 'warn',
-    'no-unused-vars': 'warn',
->>>>>>> 18686e2848a63a03f31efe0519cf8d249ce76d3b
   },
   settings: {
     react: {
