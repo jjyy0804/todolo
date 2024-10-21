02089e1d01364fcae7135b50191c0b0fa3fa91b0
ara jo <ara.jo.song@gmail.com>
2024-10-21 11:36:55 +0900

    Fix: profile display error

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -11,9 +11,9 @@ export default function ResetPassword() {
   const { token } = useParams();
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('비밀번호를 재설정 해주세요');
   const [errorMessage, setErrorMessage] = useState('');
-  const [disabled, setDisabled] = useState(false);
+  const [disabled, setDisabled] = useState(true);
 
   useEffect(() => {
     if (!token) {
@@ -23,25 +23,37 @@ export default function ResetPassword() {
         navigate(ROUTE_LINK.REQ_RESET_PASSWORD.link);
       }, 2000);
     }
-  }, [token]);
+  }, [token, navigate]);
 
-  const handleConfirmPasswordChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setPasswordConfirm(e.target.value);
-    // 비밀번호 형식 검사
+  useEffect(() => {
+    // Enable the button only when both passwords are valid and matching
+    if (
+      password &&
+      passwordConfirm &&
+      password === passwordConfirm &&
+      !errorMessage
+    ) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [password, passwordConfirm, errorMessage]);
 
+  const checkValidPassword = () => {
     const passwordRegex =
       /^(?=.*[!@#$%^&*()_\-+=])[a-zA-Z0-9!@#$%^&*()_\-+=]{1,10}$/;
     if (!passwordRegex.test(password)) {
       setErrorMessage('비밀번호는 특수문자를 포함하고 10자 이내여야 합니다.');
-      return;
+    } else {
+      setErrorMessage('');
     }
+  };
 
-    // 비밀번호 확인
+  const checkPassword = () => {
     if (password !== passwordConfirm) {
       setErrorMessage('비밀번호가 일치하지 않습니다.');
-      return;
+    } else if (!errorMessage) {
+      setDisabled(false); // Allow password reset if passwords match
     }
   };
 
@@ -62,25 +74,18 @@ export default function ResetPassword() {
       // 재설정 후 로그인 페이지로 이동
       setTimeout(() => {
         window.close();
-      }, 2000);
+      }, 3000);
     } catch (error) {
       console.error('Error requesting reset password :', error);
       setMessage('비밀번호 재설정 요청에 실패했습니다. 다시 시도해주세요.');
-      // 실패 후 메인 페이지로 이동
-      // setTimeout(() => {
-      //   navigate(ROUTE_LINK.LANDING.link);
-      // }, 2000);
     }
   };
 
   return token ? (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      {/** 요소 담는 흰색 컨테이너 박스 */}
       <div className="flex flex-col items-center justify-center w-[700px] h-[500px] p-6 bg-white shadow-lg rounded-[10px]">
         <img src={logo} alt="logo" className="mb-1 w-[364px] h-[118px]" />
-        <p className="mb-6 text-primary text-[16px] font-bold">
-          비밀번호를 재설정 해주세요
-        </p>
+        <p className="mb-6 text-primary text-[16px] font-bold">{message}</p>
 
         <div className="mb-4">
           <label className="text-sm font-medium text-softgray">
@@ -94,6 +99,7 @@ export default function ResetPassword() {
             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
               setPassword(e.target.value)
             }
+            onBlur={checkValidPassword}
           />
         </div>
 
@@ -106,16 +112,18 @@ export default function ResetPassword() {
             className="mt-1 block w-full border border-softgray rounded-md shadow-sm p-2 focus:border-primary focus:outline-none"
             placeholder="비밀번호 확인"
             value={passwordConfirm}
-            onChange={handleConfirmPasswordChange}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPasswordConfirm(e.target.value)
+            }
+            onBlur={checkPassword}
           />
           {password && passwordConfirm && password === passwordConfirm && (
             <FaCheckCircle className="absolute right-3 top-9 text-green-500" />
           )}
         </div>
 
-        {/** 상태 알려주는 메세지 */}
         <p className="block text-sm font-medium text-softgray mt-3 mb-0">
-          {message}
+          {errorMessage}
         </p>
 
         <div className="flex justify-center gap-4 mt-5">
@@ -129,7 +137,7 @@ export default function ResetPassword() {
           <button
             type="submit"
             disabled={disabled}
-            className="py-2 px-4 bg-primary text-white rounded-lg shadow-sm text-sm hover:bg-hoverprimary"
+            className="py-2 px-4 bg-primary text-white rounded-lg shadow-sm text-sm disabled:bg-softgray hover:bg-hoverprimary"
             onClick={handleChangeButtonClick}
           >
             변경하기
